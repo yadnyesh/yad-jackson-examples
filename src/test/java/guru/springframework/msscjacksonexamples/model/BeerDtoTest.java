f9bd6d516c363eb605504ccedb0b92e88289ddd3
Yadnyesh <yadnyesh.juvekar@gmail.com>
2020-04-04 20:49:16 +0530

    JSON Serializer and DeSerializer

--- src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
@@ -25,10 +25,9 @@ class BeerDtoTest extends BaseTest{
 	
 	@Test
 	void TestDeserialize() throws IOException {
-		String json = "{\"beerId\":\"ee3d6198-68f3-4c6e-a11b-54bce228f765\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123123123,\"price\":12.99,\"createdDate\":\"2020-04-04T20:14:37.794006+05:30\",\"lastUpdatedDate\":\"2020-04-04T20:14:37.794068+05:30\"}";
+		String json = "{\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123123123,\"price\":\"12.99\",\"createdDate\":\"2019-06-03T21:01:53-0400\",\"lastUpdatedDate\":\"2019-06-03T21:01:53.628287-04:00\",\"myLocalDate\":\"20190603\",\"beerId\":\"8ed4c7eb-ef3a-437e-823e-a26497ed7e71\"}\n";
 		BeerDto beerDto = objectMapper.readValue(json, BeerDto.class);
 		log.info(beerDto.toString());
-		
 	}
 	
 }
